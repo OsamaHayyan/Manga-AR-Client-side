3cb791a843bdba8227a8b042c3c13aeac7bce044
OsamaHayyan <sonikx2011@gmail.com>
2022-02-16 17:52:12 +0200

    add server side rendering to news component

--- pages/news.js
+++ pages/news.js
@@ -3,3 +3,23 @@ import React from "react";
 export default function News() {
   return <div>News</div>;
 }
+
+export async function getServerSideProps(context) {
+  try {
+    let DataExist = true;
+    const news = await fetch("http://localhost:8080/news/");
+    const newsData = await news.json();
+
+    if (newsData.length == 0) {
+      DataExist = false;
+    }
+    return {
+      props: { newsData }, // will be passed to the page component as props
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: { FetchError: "Fetch Error" },
+    };
+  }
+}
