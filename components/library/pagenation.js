bf21b2453ae0b48a4ae7de4205915b3e196a38c4
OsamaHayyan <sonikx2011@gmail.com>
2022-02-17 23:46:36 +0200

    fixed pagenation - used page state instead of useRef

--- components/library/pagenation.js
+++ components/library/pagenation.js
@@ -3,51 +3,38 @@ import React, { useRef, useState } from "react";
 import { Container } from "react-bootstrap";
 import * as pagenation from "./pagenation.module.css";
 
-export default function Pagenation({ handlePage, mangaPages, sort }) {
+export default function Pagenation({ handlePage, mangaPages, sort, page }) {
   const router = useRouter();
-  const page = useRef(1);
-  if (!router.query.page) {
-    page.current = 1;
-  } else if (!router.query.page && page.current != 1) {
-    page.current = 1;
-  }
 
   const handleNext = (e) => {
     e.preventDefault();
-    if (page.current >= mangaPages) return;
-    page.current++;
-    handlePage({ ...sort, page: page.current });
-    router.replace(`/library?page=${page.current}`);
+    if (page >= mangaPages) return;
+    handlePage({ ...sort, page: page + 1 });
+    router.replace(`/library?page=${page}`);
   };
 
   const handleLast = (e) => {
     e.preventDefault();
-    if (page.current === 1) return false;
-    // if (!router.query.page) return (page.current = 1);
-    page.current--;
-    handlePage({ ...sort, page: page.current });
-    router.replace(`/library?page=${page.current}`);
+    if (page === 1) return false;
+    handlePage({ ...sort, page: page - 1 });
+    router.replace(`/library?page=${page}`);
   };
   return (
     <>
       <div className={pagenation.container}>
         <span
-          href={`/library?page=${page.current}`}
+          href={`/library?page=${page}`}
           className={`${pagenation.span} ${
-            router.query.page <= 1 || page.current === 1
-              ? pagenation.un_click_last
-              : ""
+            page <= 1 ? pagenation.un_click_last : ""
           } ${pagenation.last}`}
           onClick={handleLast}
         >
           Last
         </span>
         <span
-          href={`/library?page=${page.current}`}
+          href={`/library?page=${page}`}
           className={`${pagenation.span} ${
-            router.query.page >= mangaPages || page.current >= mangaPages
-              ? pagenation.un_click_next
-              : ""
+            page >= mangaPages ? pagenation.un_click_next : ""
           }`}
           onClick={handleNext}
         >
