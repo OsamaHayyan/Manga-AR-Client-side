b5c8df3af5cfe71f89433ae1cf9c2fc43a313505
OsamaHayyan <sonikx2011@gmail.com>
2022-02-18 15:33:48 +0200

    added  SSR to manga page

--- pages/series/[manga]/[mangaId].js
+++ pages/series/[manga]/[mangaId].js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState, useRef } from "react";
 import { IconContext } from "react-icons";
 
-import * as manga from "../../styles/manga_page.module.css";
-import Chapters from "../../components/series/Chapters";
-import Details from "../../components/series/Details";
+import * as manga from "../../../styles/manga_page.module.css";
+import Chapters from "../../../components/series/Chapters";
+import Details from "../../../components/series/Details";
 import { FaArrowAltCircleUp } from "react-icons/fa";
 import { animateScroll } from "react-scroll";
-import { Image } from "react-bootstrap";
-import Favorite from "../../components/series/favorite";
+import Favorite from "../../../components/series/favorite";
 
-export default function Manga() {
+export default function Manga({ chapter }) {
   const [hide, setHide] = useState(true);
   const bannar = "/images/banner.jpg";
 
@@ -44,4 +43,27 @@ export default function Manga() {
       />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    let DataExist = true;
+    const chapter = await fetch(
+      "http://localhost:8080/mangas/manga/620d75900b812cc56875e40b"
+    );
+    const chapterData = await chapter.json();
+
+    if (chapter.message) {
+      DataExist = false;
+    }
+
+    return {
+      props: { chapter: chapterData, DataExist }, // will be passed to the page component as props
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: { FetchError: "Fetch Error" },
+    };
+  }
 }
