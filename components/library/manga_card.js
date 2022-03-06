c88bd7b47cf80e3f9fb21bd80a9ae56d8703e350
OsamaHayyan <sonikx2011@gmail.com>
2022-03-06 18:46:37 +0200

    changed some classes name in card component

--- components/library/manga_card.js
+++ components/library/manga_card.js
@@ -1,7 +1,7 @@
 import React from "react";
-import * as manga_card from "./manga_card.module.css";
+import * as card from "./manga_card.module.css";
 import { Container } from "react-bootstrap";
-import { TiEye } from "react-icons/ti";
+import { TiEye, TiCalendar } from "react-icons/ti";
 import { useRouter } from "next/dist/client/router";
 
 export default function MangaCard({ manga, newsData }) {
@@ -17,18 +17,18 @@ export default function MangaCard({ manga, newsData }) {
         let data = manga.map((m) => (
           <div
             key={m._id}
-            className={manga_card.child}
+            className={card.child}
             onClick={() => handleNavigation(m._id, m.title)}
           >
             <img
               id={m._id}
               src={`http://localhost:8080/${m.image}`}
-              className={manga_card.cardImage}
+              className={card.cardImage}
             />
 
-            <div key={m._id + "child"} className={manga_card.cardData}>
-              <h3 className={manga_card.cardTitle}>{m.title}</h3>
-              <div className={manga_card.cardViews}>
+            <div key={m._id + "child"} className={card.cardData}>
+              <h3 className={card.cardTitle}>{m.title}</h3>
+              <div className={card.cardBottom}>
                 <div className="d-flex flex-row align-items-center">
                   <TiEye
                     size="20px"
@@ -45,16 +45,25 @@ export default function MangaCard({ manga, newsData }) {
       }
     } else if (newsData) {
       let data = newsData.map((news) => (
-        <div key={news._id} className={manga_card.child}>
+        <div key={news._id} className={card.child}>
           <img
             id={news._id}
             src={`http://localhost:8080/${news.poster}`}
-            className={manga_card.cardImage}
+            className={card.cardImage}
           />
-          <div key={news._id + "child"} className={manga_card.cardData}>
-            <h3 className={manga_card.cardTitle}>{news.title}</h3>
-
-            <p className={manga_card.newsTopic}>{news.topic}</p>
+          <div key={news._id + "child"} className={card.cardData}>
+            <h3 className={card.cardTitle}>{news.title}</h3>
+            <p className={card.newsTopic}>{news.topic}</p>
+            <div className={card.cardBottom}>
+              <div className="d-flex flex-row align-items-center">
+                <TiCalendar
+                  size="20px"
+                  color="#ff5b3b"
+                  style={{ marginRight: "5px" }}
+                />
+                <p style={{ lineHeight: 0 }}>{news.createdAt.split("T")[0]}</p>
+              </div>
+            </div>
           </div>
         </div>
       ));
@@ -63,7 +72,7 @@ export default function MangaCard({ manga, newsData }) {
     }
   };
   return (
-    <Container fluid className={manga_card.container}>
+    <Container fluid className={card.container}>
       <MangaData />
     </Container>
   );
