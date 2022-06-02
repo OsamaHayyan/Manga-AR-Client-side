084f5948d39468fa305c8dfb00bd3ab3c7d022d6
osamahayyan <sonikx2011@gmail.com>
2022-06-02 20:17:38 +0200

    removed logs from middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,9 +4,7 @@ export default function middleware(req) {
   const { cookies, url } = req;
   if (url.includes("/user/login" || "/user/signup")) {
     const { logged_in } = cookies;
-    console.log(cookies);
     if (logged_in === "true") {
-      console.log("test");
       return NextResponse.redirect("/library");
     }
   }
