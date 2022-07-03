d7bb4467855e32e00397e2c94cc463b364b9b2b4
osamahayyan <sonikx2011@gmail.com>
2022-07-03 18:12:11 +0200

    fixed cookie error on refresh while user logged in by making custom _app and sending cookies from server to component

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "react-toastify/dist/ReactToastify.css";
 import { useState } from "react";
-import Cookies from "universal-cookie";
+import App from "next/app";
 import Navbar from "../components/navbar/navbar";
 import "../styles/globals.css";
+import Layout from "../components/layout";
 import { ToastContainer } from "react-toastify";
 
-function MyApp({ Component, pageProps }) {
-  const cookies = new Cookies();
-  const [checkLogin, setLogin] = useState(
-    cookies.get("logged_in") === "true" ? true : false
-  );
+function MyApp({ Component, pageProps, request }) {
+  console.log(request);
+  const [checkLogin, setLogin] = useState(request);
   return (
     <>
       <Navbar checkLogin={checkLogin} handleLoginState={setLogin} />
@@ -19,5 +18,15 @@ function MyApp({ Component, pageProps }) {
     </>
   );
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+  //getInitialProps runs on both server-side and client-side.
+  //For that reason you need to add a check before accessing things in appContext.ctx.req, as req will not be defined on the client.
+  const req =
+    (await appContext.ctx.req?.cookies["logged_in"]) === "true" ? true : false;
+  return { ...appProps, request: req };
+};
 
 export default MyApp;
