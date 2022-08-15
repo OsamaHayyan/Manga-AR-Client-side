08472ab153935d5c8aef39121c6287cdd5956814
osamahayyan <sonikx2011@gmail.com>
2022-08-15 13:47:12 +0200

    search page api

--- pages/search.js
+++ pages/search.js
@@ -1,27 +1,53 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import * as searchStyle from "../styles/search.module.css";
 export default function Search() {
+  const [results, setResults] = useState([]);
+  let timer;
+  const handleSearch = async (e) => {
+    try {
+      e.preventDefault();
+      clearTimeout(timer);
+      timer = setTimeout(async () => {
+        const input = e.target.value;
+        if (input.length < 3) return;
+
+        const result = await (
+          await axios.post("http://localhost:8080/mangas/search-manga/", {
+            query: input,
+          })
+        ).data;
+
+        setResults(result);
+      }, 300);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <div className={searchStyle.searchContainer}>
         <div className={searchStyle.searchInputContainer}>
           <AiOutlineSearch />
-          <input type={"text"} className={searchStyle.searchInput} />
+          <input
+            type={"text"}
+            className={searchStyle.searchInput}
+            onInput={handleSearch}
+          />
         </div>
 
         <p className={searchStyle.cancel}>cancel</p>
       </div>
       <ul className={searchStyle.searchList}>
-        <li className={searchStyle.searchItem}>
-          <img
-            src={
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQwWMJbZoZ26ZyYB8M-1e7OLBVUWXRLNSO6A&usqp=CAU"
-            }
-          />
+        {results.map((manga) => (
+          <li key={manga._id} className={searchStyle.searchItem}>
+            <img src={`http://localhost:8080/${manga.image}`} />
 
-          <p>manga name</p>
-        </li>
+            <p>{manga.title}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
