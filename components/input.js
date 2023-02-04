a2c8e8d562af36246f2ac79588f7b4c575ebb834
OsamaHayyan <sonikx2011@gmail.com>
2023-02-04 21:18:11 +0200

    add validation in input components

--- components/input.js
+++ components/input.js
@@ -3,26 +3,41 @@ import { forwardRef } from "react";
 import * as inputStyle from "../styles/input.module.css";
 const Input = forwardRef(
   (
-    { Icon, lastIcon = null, name, type, placeholder, required, style },
+    {
+      Icon,
+      lastIcon = null,
+      name,
+      type,
+      placeholder,
+      validation,
+      validationText,
+      required,
+      style,
+    },
     ref
   ) => {
     return (
-      <div className={inputStyle.inputContainer} style={style}>
-        {Icon}
-        <input
-          className={inputStyle.inputs}
-          style={
-            lastIcon
-              ? { width: `calc(100% - ${72 - lastIcon?.width}px)` }
-              : null
-          }
-          name={name}
-          type={type}
-          placeholder={placeholder}
-          ref={ref}
-          required={required ? true : false}
-        />
-        {lastIcon?.icon}
+      <div>
+        <div className={inputStyle.inputContainer} style={style}>
+          {Icon}
+          <input
+            className={inputStyle.inputs}
+            style={
+              lastIcon
+                ? { width: `calc(100% - ${72 - lastIcon?.width}px)` }
+                : null
+            }
+            name={name}
+            type={type}
+            placeholder={placeholder}
+            ref={ref}
+            required={required ? true : false}
+          />
+          {lastIcon?.icon}
+        </div>
+        {validation && (
+          <p className={inputStyle.validation}>{validationText}</p>
+        )}
       </div>
     );
   }
