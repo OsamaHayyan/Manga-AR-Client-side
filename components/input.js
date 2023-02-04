1e39d9402f64654e35669184c9d84bbe0e8acd52
OsamaHayyan <sonikx2011@gmail.com>
2023-02-04 13:46:10 +0200

    passed style as props

--- components/input.js
+++ components/input.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { forwardRef } from "react";
 import * as inputStyle from "../styles/input.module.css";
-const Input = forwardRef(({ Icon, name, type, placeholder, required }, ref) => {
-  return (
-    <div className={inputStyle.inputContainer}>
-      {Icon}
-      <input
-        className={inputStyle.inputs}
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        ref={ref}
-        required={required ? true : false}
-      />
-    </div>
-  );
-});
+const Input = forwardRef(
+  ({ Icon, name, type, placeholder, required, style }, ref) => {
+    return (
+      <div className={inputStyle.inputContainer} style={style}>
+        {Icon}
+        <input
+          className={inputStyle.inputs}
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          ref={ref}
+          required={required ? true : false}
+        />
+      </div>
+    );
+  }
+);
 
 export default Input;
