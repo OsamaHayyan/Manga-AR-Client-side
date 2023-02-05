acc5431bb030ea448937be84e2e499a71a1a9337
OsamaHayyan <sonikx2011@gmail.com>
2023-02-06 01:30:28 +0200

    passed className, style to props

--- components/manga_form.js
+++ components/manga_form.js
@@ -2,13 +2,21 @@ import { Button } from "@mui/material";
 import React from "react";
 import * as mangaFromStyle from "../styles/mangaForms.module.css";
 
-export default function MangaForm({ formName, onSubmit, children, disable }) {
+export default function MangaForm({
+  formName,
+  onSubmit,
+  children,
+  disable,
+  style,
+  className,
+}) {
   return (
     <form
       method="POST"
       encType="multipart/form-data"
-      className={mangaFromStyle.formStyle}
+      className={`${mangaFromStyle.formStyle} ${className}`}
       onSubmit={onSubmit}
+      style={style}
     >
       <div className={mangaFromStyle.container}>
         <h1 className={mangaFromStyle.formHeader}>{formName}</h1>
