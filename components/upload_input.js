2472082d28469b6c309a70a1966be9fd589d4e42
OsamaHayyan <sonikx2011@gmail.com>
2023-02-10 21:03:28 +0200

    added react-hook-form to upload_input

--- components/upload_input.js
+++ components/upload_input.js
@@ -1,37 +1,56 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import inputStyle from "../styles/upload_input.module.css";
 
 export default function InputUpload({
-  id,
+  id = "file",
   icon,
   handleFileInput,
   name,
   fileName,
   validation,
   register,
-  error,
   validationStyle,
   validationText,
+  errors,
   accept,
   calssName,
   lastIcon,
 }) {
+  let [selectedFile, setSelectedFile] = useState();
+  const handleImage = (e) => {
+    setSelectedFile(e.target.files[0]?.name);
+  };
   return (
     <div>
       <label htmlFor={id}>
         <div className={`${inputStyle.inputFile} ${calssName}`}>
           {icon}
-          <p>{fileName}</p>
+          <p>{selectedFile ? selectedFile : fileName}</p>
           {lastIcon}
         </div>
         {register ? (
           <>
             <input
-              {...register(name, validation)}
+              {...register(name, {
+                ...validation,
+                validate: (file) => {
+                  if (
+                    file.length > 0 &&
+                    (!file[0].type.includes("image") ||
+                      file[0].size * Math.pow(10, -6) > 10)
+                  ) {
+                    setSelectedFile(null);
+                    document.querySelector(`#${id}`).value = null;
+                    return false;
+                  }
+                },
+                onChange: handleImage,
+              })}
               id={id}
               type="file"
               accept={accept}
-              onChange={handleFileInput}
               style={{ display: "none" }}
             />
           </>
@@ -45,9 +64,9 @@ export default function InputUpload({
           />
         )}
       </label>
-      {error && (
+      {errors && (
         <p className={inputStyle.validation} style={validationStyle}>
-          {validationText}
+          {errors.required ? validationText : "maximum image size is 10MB"}
         </p>
       )}
     </div>
