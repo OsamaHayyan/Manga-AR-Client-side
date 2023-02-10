10e761b6aa0d905fd37028d7e738e81ba644b202
OsamaHayyan <sonikx2011@gmail.com>
2023-02-10 21:02:38 +0200

    made auto_complete_input more dynamic

--- components/auto_complete_input.js
+++ components/auto_complete_input.js
@@ -1,6 +1,9 @@
-import React from "react";
-import * as inputStyle from "../styles/input.module.css";
+import React, { useRef, useState } from "react";
+import { useEffect } from "react";
+import * as inputStyle from "../styles/autoComplete.module.css";
+import { DownArrow, Person } from "./icons";
 export default function AutoComplete({
+  id,
   Icon,
   lastIcon = null,
   name,
@@ -13,33 +16,166 @@ export default function AutoComplete({
   style,
   className,
   validationStyle,
-  onChange,
+  handleValue,
   onBlur,
-  register,
+  options,
+  filterOptions,
+  onInput,
+  accessedDataName,
+  accessedValueName,
+  multiple,
 }) {
+  const compare = (a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  };
+  const inputList = useRef();
+  const selectedRef = React.createRef();
+  const [searchedOptions, setSearchedOptions] = useState(options);
+  const [listHide, setListHide] = useState(true);
+  const [selectedOption, setSelectedOption] = useState([]);
+  const onClickOption = (e) => {
+    const data = JSON.parse(e.target.dataset.value);
+    let newData;
+    if (multiple) {
+      const newOptions = options.filter(
+        (option) => option[accessedValueName] != data[accessedValueName]
+      );
+
+      options.length != 0 &&
+        filterOptions((allData) => {
+          return { ...allData, [id]: newOptions };
+        });
+      if (selectedOption.some(({ _id }) => _id == data._id)) return;
+      newData = [...selectedOption, data];
+      setSelectedOption(newData);
+    } else {
+      newData = data;
+      setSelectedOption(newData);
+    }
+    handleValue(newData);
+    inputList.current.value = null;
+  };
+  const handleSearch = (e) => {
+    setSearchedOptions(
+      options.filter((opt) =>
+        opt[accessedDataName]
+          .toLowerCase()
+          .includes(e.target.value?.toLowerCase())
+      )
+    );
+  };
+
+  const handleRemoveSelected = async (e) => {
+    const data = await selectedRef.current?.dataset.value;
+    const element = await JSON.parse(data);
+    const newSelecedOption = selectedOption.filter(
+      (sl) => sl._id != element._id
+    );
+    setSelectedOption(newSelecedOption);
+    const newOptions = [...options, element].sort((a, b) =>
+      compare(a[accessedDataName], b[accessedDataName])
+    );
+    filterOptions((oldVal) => {
+      return { ...oldVal, [id]: newOptions };
+    });
+  };
+  useEffect(() => {
+    setSearchedOptions(options);
+  }, [options]);
   return (
     <div>
-      <div
-        className={`${inputStyle.inputContainer} ${className}`}
-        style={style}
-      >
+      <div className={inputStyle.boxContainer}>
         {Icon}
-        <input
-          className={inputStyle.inputs}
-          style={
-            lastIcon
-              ? { width: `calc(100% - ${72 - lastIcon?.width}px)` }
-              : null
-          }
-          onChange={onChange}
-          onBlur={onBlur}
-          type={type}
-          placeholder={placeholder}
-          required={required ? true : false}
-          {...register(name, validation)}
-        />
-        {lastIcon?.icon}
+        {Array.isArray(selectedOption) ? (
+          selectedOption?.map((option, i) => (
+            <li
+              key={i}
+              className={inputStyle.selectedOption}
+              data-value={JSON.stringify(option)}
+              ref={selectedRef}
+            >
+              <span>
+                {accessedDataName ? option[accessedDataName] : option}
+              </span>
+              <DownArrow
+                width="24px"
+                height="24px"
+                className={inputStyle.removeIcon}
+                onClick={handleRemoveSelected}
+              />
+            </li>
+          ))
+        ) : (
+          <li className={inputStyle.selectedOption}>
+            <span>
+              {accessedDataName
+                ? selectedOption[accessedDataName]
+                : selectedOption}
+            </span>
+            <DownArrow
+              width="24px"
+              height="24px"
+              className={inputStyle.removeIcon}
+            />
+          </li>
+        )}
+        <div
+          id={id}
+          className={`${inputStyle.inputContainer} ${className}`}
+          style={style}
+        >
+          <input
+            className={inputStyle.inputs}
+            type={type}
+            placeholder={placeholder}
+            required={required ? true : false}
+            data-value={selectedOption}
+            ref={inputList}
+            onFocus={() => {
+              setListHide(false);
+            }}
+            onBlur={() => setListHide(true)}
+            onChange={handleSearch}
+            onInput={onInput}
+          />
+          <DownArrow
+            width="32px"
+            height="32px"
+            className={!listHide ? inputStyle.reflectArrow : null}
+          />
+        </div>
       </div>
+
+      <ul
+        className={listHide ? inputStyle.hideList : inputStyle.showList}
+        style={{
+          background: "#000000",
+          paddingLeft: "8px",
+          transformOrigin: "top",
+          transition: "max-height 300ms ease-out",
+          overflowY: "auto",
+        }}
+      >
+        {searchedOptions?.map((option, i) => (
+          <li
+            key={i}
+            data-value={JSON.stringify(option)}
+            onMouseDown={onClickOption}
+            style={{
+              fontWeight: "400",
+              fontSize: "32px",
+              color: "#FFFFFF",
+              height: "48px",
+              lineHeight: "48px",
+              cursor: "pointer",
+            }}
+          >
+            {accessedDataName ? option[accessedDataName] : option}
+          </li>
+        ))}
+      </ul>
       {error && (
         <p className={inputStyle.validation} style={validationStyle}>
           {validationText}
