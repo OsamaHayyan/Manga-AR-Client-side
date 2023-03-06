b871b10f5ae578c9dd26b9b8afd120ec61da54f7
osamahayyan <osamahayyan1567@gmail.com>
2023-03-07 00:54:49 +0200

    new manga_card design

---
+++ components/library/manga_card.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import card from "./manga_card.module.css";
+import { Container } from "react-bootstrap";
+import { TiEye, TiCalendar } from "react-icons/ti";
+import { useRouter } from "next/dist/client/router";
+import { manga } from "../../util/interfaces";
+import Image from "next/image";
+import Rate from "../rate";
+
+export default function MangaCard({ manga }: { manga: manga }) {
+  const router = useRouter();
+
+  const handleNavigation = (_id, title) => {
+    return router.push(`series/${title}/${_id}`);
+  };
+
+  return (
+    <div className={card.container}>
+      <section className={card.imageSection}>
+        <Image
+          src={`http://localhost:8080/${manga.image}`}
+          alt=""
+          fill
+          style={{ objectFit: "cover", borderRadius: "5px" }}
+        />
+      </section>
+      <section className={card.contentSection}>
+        <p className={card.title}>{manga.title}</p>
+        <div>
+          <p className={card.category}>
+            {manga.category.map((cat) => cat.category).join(", ")}
+          </p>
+          <Rate rate={manga.rate} width={"24px"} height={"24px"} />
+          <div>
+            <p className={card.story}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reiciendis, inventore assumenda repellat vel, eum nam est beatae
+              dolorem fuga consequatur facere sit. Quaerat adipisci itaque
+              voluptate alias perspiciatis dolores dicta?
+            </p>
+            <p
+              className={card.story}
+              style={{ textDecorationLine: "underline" }}
+            >
+              Tap to continue
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
