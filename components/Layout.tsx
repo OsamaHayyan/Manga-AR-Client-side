217a05b36653580b42a823762aaf2b63f2141ef4
osamahayyan <osamahayyan1567@gmail.com>
2023-04-23 15:50:15 +0200

    changed Layout component to TSX

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,18 @@ import { useRouter } from "next/dist/client/router";
 import { ToastContainer } from "react-toastify";
 import { Poppins } from "@next/font/google";
 import Navbar from "./navbar/navbar";
+import { Dispatch, PropsWithChildren, SetStateAction } from "react";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
-export default function Layout({ children, checkLogin, setLogin }) {
+type Props = PropsWithChildren<{
+  checkLogin: boolean;
+  setLogin: Dispatch<SetStateAction<boolean>>;
+}>;
+export default function Layout({ children, checkLogin, setLogin }: Props) {
   const { pathname } = useRouter();
   return (
     <>
