a7e3fcea6bf00ac88c4f4eeebccaa0dd2e3bc039
osamahayyan <osamahayyan1567@gmail.com>
2023-04-23 18:50:37 +0200

    made layout component for login and signup component

---
+++ components/user/RegisterLayout.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+import userForm from "../../styles/userForm.module.css";
+import backgroundImage from "../../public/images/userFormBackground.jpg";
+import { useRouter } from "next/router";
+
+export default function User({ children }: { children: React.ReactNode }) {
+  const route = useRouter();
+
+  return (
+    <div className={userForm.container}>
+      <div className={userForm.imageSide}>
+        <Image
+          src={backgroundImage}
+          fill
+          className={userForm.backgroundImage}
+          alt="Background image"
+        />
+        <button
+          className={
+            route.pathname === "/user/login"
+              ? `${userForm.selected} ${userForm.loginBtnPosition}`
+              : null
+          }
+          type="button"
+          onClick={() => route.push("login")}
+        >
+          <div
+            className={
+              route.pathname === "/user/login"
+                ? userForm.textSelected
+                : `${userForm.textNotSelected} ${userForm.loginTextPosition}`
+            }
+          >
+            LOGIN
+          </div>
+        </button>
+        <button
+          className={
+            route.pathname === "/user/signup"
+              ? `${userForm.selected} ${userForm.signBtnPosition}`
+              : null
+          }
+          type="button"
+          onClick={() => route.push("signup")}
+        >
+          <div
+            className={
+              route.pathname === "/user/signup"
+                ? userForm.textSelected
+                : `${userForm.textNotSelected} ${userForm.signTextPosition}`
+            }
+          >
+            SIGN UP
+          </div>
+        </button>
+        <div className={userForm.background}></div>
+      </div>
+      <div className={userForm.formSide}>{children}</div>
+    </div>
+  );
+}
