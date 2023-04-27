bfa913a8034536dcd3c9d6c36c03ddc88e19d902
osamahayyan <osamahayyan1567@gmail.com>
2023-04-27 15:19:29 +0200

    removed unused props and getinitialstate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,25 +5,12 @@ import App from "next/app";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps, request }) {
-  const [checkLogin, setLogin] = useState<boolean>(request);
-
+function MyApp({ Component, pageProps }) {
   return (
-    <Layout checkLogin={checkLogin} setLogin={setLogin}>
-      <Component handleLoginState={setLogin} {...pageProps} />
+    <Layout>
+      <Component {...pageProps} />
     </Layout>
   );
 }
-
-MyApp.getInitialProps = async (appContext) => {
-  // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext);
-  //getInitialProps runs on both server-side and client-side.
-  //For that reason you need to add a check before accessing things in appContext.ctx.req, as req will not be defined on the client.
-  const req = (await appContext.ctx.req?.cookies["access_token"])
-    ? true
-    : false;
-  return { ...appProps, request: req };
-};
 
 export default MyApp;
