3e67ea0223cb0e429ec7cb2abdf7bf0aebe2902e
osamahayyan <osamahayyan1567@gmail.com>
2023-08-01 12:17:08 +0300

    fixed env for PROTOCOL

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     domains: [process.env.NEXT_PUBLIC_HOSTURL],
     remotePatterns: [
       {
-        protocol: NEXT_PUBLIC_PROTOCOL,
+        protocol: process.env.NEXT_PUBLIC_PROTOCOL,
         hostname: process.env.NEXT_PUBLIC_HOSTNAME,
       },
     ],
